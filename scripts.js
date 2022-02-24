91bb32abbeeeaecdd723a297d252ef58d57de70f
Christian <christianbaus@hotmail.com>
2022-02-24 19:34:29 -0300

    primer commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function saludar(){
+    alert("Chau mundo");
+}
+
+
+const boton = document.getElementById('btn');
+
+boton.addEventListener('click', ()=>{ alert("Soy un manejador de eventos")} );
+
